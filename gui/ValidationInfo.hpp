7893e198152184b44d298ca693af20d2883eeb83
AlbertSamvelyan <albertsamvelyan@gmail.com>
2017-06-15 12:25:44 +0400

    updated: Input validator class modified, ValidationInfo struct added

---
+++ gui/ValidationInfo.hpp
@@ -0,0 +1,24 @@
+#ifndef VALIDATIONINFO_HPP
+#define VALIDATIONINFO_HPP
+
+namespace ValidationInfo
+{
+    using String = std::string;
+    
+    const SizeType minimalSymbols = 2;
+    const SizeType maximumSymbols = 20;
+    const char underscore = '_';
+
+    const String emptyField = "Empty field.";
+    const String loggedUser = "Logged user";
+    const String validLogPass = "Valid log pass";
+    const String validAccount = "Valid account";
+    const String validMaxLength = "Valid max length";
+    const String validMinLength = "Valid min length";
+    const String validName = "Valid name";
+    const String takenLogin = "Taken login";
+    const String mismatchPass = "Mismatching pass";
+};
+#endif
+
+
