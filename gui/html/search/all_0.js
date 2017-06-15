5daab1a96b11dfdc1a68bd63c21d6bba0f78d084
aram96 <tsatryanaram@gmail.com>
2017-06-15 14:42:46 +0400

    updated: some unimportant changes

---
+++ gui/html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['checkpassword',['checkPassword',['../classLoginWindow.html#ad5a16b9244af77d36f98a4f48d3d423d',1,'LoginWindow']]],
+  ['closeregwindow',['closeRegWindow',['../classLoginWindow.html#a1d429ebfb0f2ebb21591b209da5d4ecb',1,'LoginWindow']]]
+];
