78c45a36fc60557f85c5160534eb5c33a740eb3c
aram96 <tsatryanaram@gmail.com>
2017-06-21 13:34:17 +0400

    Added Doxygen

--- core/ClientUser.hpp
+++ core/ClientUser.hpp
@@ -1,3 +1,12 @@
+
+/** 
+*  @file    ClientUser.hpp
+*  @author  GRI Team
+*  @date    06/21/2017  
+*  @version 1.0 
+*  @brief ...
+*/
+
 #ifndef CLIENTUSER_HPP
 #define CLIENTUSER_HPP
 
@@ -7,32 +16,109 @@
 
 #include "ExtractWord.hpp"
 
+/**
+*  @brief ClientUser Class  
+*/  
 class ClientUser
 {
 	public:
 		using String = std::string;
 		using Messages = std::list<String>;
 
+                /** 
+                *   @brief ClientUser is constructor for Class ClientUser   
+                */  
 		ClientUser();
+                /** 
+                *   @brief  ...
+                *   @param...  
+                *   @return ...
+                */  
 		explicit ClientUser(String&);
 
-		bool fromString(String&);
+		/** 
+                *   @brief  ...
+                *   @param  ...
+                *   @return bool parametr
+                */  
+                bool fromString(String&);
 
+                /** 
+                *   @brief  ...
+                *   @param  ...
+                *   @return void
+                */  
 		void addMessage(const String&);
 
+                /** 
+                *   @brief  ...
+                *   @param  no parametrs 
+                *   @return String 
+                */  
 		String toString();
 
+                /** 
+                *   @brief  ...
+                *   @param  no parametrs 
+                *   @return String
+                */  
 		String getLogin() const;
+
+                /** 
+                *   @brief  ...
+                *   @param  no parametrs 
+                *   @return String
+                */  
 		String getName() const;
+
+                /** 
+                *   @brief  ....
+                *   @param  no parametrs 
+                *   @return String
+                */  
 		String getSurname() const;
+
+                /** 
+                *   @brief  ....
+                *   @param  no parametrs 
+                *   @return bool
+                */  
 		bool getStatus() const;
+
+                /** 
+                *   @brief  ....
+                *   @param  no parametrs 
+                *   @return ??....
+                */  
 		size_t getUnreadMessagesCount() const;
+
+                /** 
+                *   @brief  ...
+                *   @param  no parametrs
+                *   @return ???...
+                */  
 		Messages& getMessages();
 
+                /** 
+                *   @brief  ....
+                *   @param  ...
+                *   @return void
+                */  
 		void setStatus(const bool);
+
+                /** 
+                *   @brief  ...
+                *   @param  ...
+                *   @return void
+                */  
 		void setUnreadMessages(const size_t);
 
-		ClientUser* getPointer();
+                /** 
+                *   @brief  ...
+                *   @param  no parametrs
+                *   @return ...
+                */      
+                ClientUser* getPointer();
 	private:
 		void operator=(const ClientUser&);
 		bool operator==(const ClientUser&);
